d1167ce6ffb5ce0a330b8c3c573fd1219705996d
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2025-11-06 20:02:37 -0600

    E: finished project

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from stats import word_count
+from stats import word_count, char_count, sorted_dictionaries
+import sys
 
 def get_book_text(filepath):
   with open(filepath) as file:
     return file.read()
 
 def main():
-  book = get_book_text("./books/frankenstein.txt")
-  num_words = word_count(book)
-  print(f"Found {num_words} total words")
-
+  if len(sys.argv) >= 2:
+    print("============ BOOKBOT ============")
+    path = sys.argv[1]
+    print(f"Analyzing book found at {path}...")
+    book = get_book_text(f"./{path}")
+    print("----------- Word Count ----------")
+    num_words = word_count(book)
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    count = char_count(book)
+    sorted_list = sorted_dictionaries(count)
+    for entry in sorted_list:
+      if entry["char"].isalpha():
+        print(f"{entry["char"]}: {entry["num"]}")
+  else:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
 def word_count(text):
   words = text.split()
   return len(words)
+
+def char_count(text):
+  char = {}
+  for c in text:
+    c = c.lower()
+    if c in char:
+      char[c] += 1
+    else:
+      char[c] = 1
+  return char
+
+def sort_on(items):
+  return items["num"]
+
+def sorted_dictionaries(character_dict):
+  sorted_list = []
+  for key in character_dict:
+    sorted_list.append({"char": key, "num": character_dict[key]})
+  sorted_list.sort(reverse=True, key=sort_on)
+  return sorted_list
+  
